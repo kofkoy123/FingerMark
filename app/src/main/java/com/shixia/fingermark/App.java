22191fb2244c1603b0e1547328cf345af144b7d3
lzrrong <125032854@qq.com>
2018-09-28 11:54:27 +0800

    指纹识别学习

---
+++ app/src/main/java/com/shixia/fingermark/App.java
@@ -0,0 +1,15 @@
+package com.shixia.fingermark;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2018/9/28.
+ */
+
+public class App extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
